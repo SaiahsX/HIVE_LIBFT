9bcfd833868563513e733ef72cf8d1f7962945e9
Oluwaseun Sunday Adewumi <Saiahs.ent@gmail.com>
2023-11-25 17:30:44 +0200

    Add files via upload

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,21 +6,24 @@
 /*   By: oadewumi <oadewumi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 15:10:21 by oadewumi          #+#    #+#             */
-/*   Updated: 2023/11/20 18:38:04 by oadewumi         ###   ########.fr       */
+/*   Updated: 2023/11/25 17:23:29 by oadewumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*	This function copies upto 'dstsize -1' characters from 'src' to 
 'dst' and null erminating the result if 'dstsize' is not zero	*/
-/*	it replaces the characters in the source & ends with a null terminator
+/*	it replaces the characters in the destination & ends with a null terminator
 which is the reaon for 'dst[i] = '\0'.	*/
 /*	The return values vary iin a sense that if th specified dstsize = 0,
 it returns the lenght of src	*/
 /*	In this function ft_strlcpy, the 'l' indicates its an integer
 return value which is the lenght of the src	*/
-/*	For some unknoownn reason my function still works if the argument size
+/*	For some unknownn reason my function still works if the argument size
 dstsize is larger than dst while the original function displays 
 overflow error. Needs more investigation	*/
+/*	upon investigating, the originl function requires a dest buffer size
+to be specified. preferbly larger than the dstsize. While my function
+somehow assumes theres enough dest buffer size. since i havent handled it	*/
 /*	This function imitates the behaviour of the standard C library strlcpy	*/
 
 #include "libft.h"
